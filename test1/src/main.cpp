b34338b1afcabd024747aebd150a49615fd85b84
Boni <bonizz@gmail.com>
2019-12-21 18:48:24 -0800

    creating device, queue, swapchain (phew - almost there)
    mostly copying/referencing https://github.com/iOrange/rtxON

--- test1/src/main.cpp
+++ test1/src/main.cpp
@@ -11,9 +11,39 @@
 const char* kAppName = "test vulkan app";
 bool kEnableValidation = true;
 
-VkInstance g_instance;
 GLFWwindow* g_window;
+VkInstance g_instance;
+VkPhysicalDevice g_physicalDevice;
+VkDevice g_device;
+uint32_t g_queueIndex;
+VkQueue g_queue;
+VkPhysicalDeviceRayTracingPropertiesNV g_rtProps;
+VkSurfaceKHR g_surface;
+VkSurfaceFormatKHR g_surfaceFormat;
+VkSwapchainKHR g_swapchain;
+std::vector<VkImage> g_swapchainImages;
+std::vector<VkImageView> g_swapchainImageViews;
 
+uint32_t kWindowWidth = 800;
+uint32_t kWindowHeight = 600;
+
+VkPresentModeKHR getPresentMode(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface)
+{
+    uint32_t presentModeCount;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(g_physicalDevice, g_surface, &presentModeCount, nullptr);
+    std::vector<VkPresentModeKHR> presentModes(presentModeCount);
+    vkGetPhysicalDeviceSurfacePresentModesKHR(g_physicalDevice, g_surface, &presentModeCount, presentModes.data());
+
+    for (auto& mode : presentModes)
+        if (mode == VK_PRESENT_MODE_IMMEDIATE_KHR)
+            return mode;
+
+    for (auto& mode : presentModes)
+        if (mode == VK_PRESENT_MODE_FIFO_RELAXED_KHR)
+            return mode;
+
+    return VK_PRESENT_MODE_FIFO_KHR;
+}
 
 bool init()
 {
@@ -28,11 +58,10 @@ bool init()
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    g_window = glfwCreateWindow(800, 600, kAppName, nullptr, nullptr);
+    g_window = glfwCreateWindow(kWindowWidth, kWindowHeight, kAppName, nullptr, nullptr);
 
     if (!g_window)
         return false;
-
 
     VkApplicationInfo appInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
     appInfo.pApplicationName = kAppName;
@@ -71,6 +100,158 @@ bool init()
 
     volkLoadInstance(g_instance);
 
+    uint32_t numPhysicalDevices = 0;
+    VK_CHECK(vkEnumeratePhysicalDevices(g_instance, &numPhysicalDevices, nullptr));
+    std::vector<VkPhysicalDevice> physicalDevices(numPhysicalDevices);
+    VK_CHECK(vkEnumeratePhysicalDevices(g_instance, &numPhysicalDevices, physicalDevices.data()));
+    g_physicalDevice = physicalDevices[0];
+
+    uint32_t queueFamilyPropertyCount;
+    vkGetPhysicalDeviceQueueFamilyProperties(g_physicalDevice, &queueFamilyPropertyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyPropertyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(g_physicalDevice, &queueFamilyPropertyCount, queueFamilyProperties.data());
+
+    g_queueIndex = ~0u;
+    for (uint32_t i = 0; i < queueFamilyPropertyCount; i++)
+    {
+        if (queueFamilyProperties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            g_queueIndex = i;
+            break;
+        }
+    }
+
+    if (g_queueIndex == ~0u)
+        return false;
+
+    const float queuePriorites[] = { 1.f };
+    VkDeviceQueueCreateInfo deviceQueueCreateInfo = { VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO };
+    deviceQueueCreateInfo.flags = 0;
+    deviceQueueCreateInfo.queueFamilyIndex = g_queueIndex;
+    deviceQueueCreateInfo.queueCount = 1;
+    deviceQueueCreateInfo.pQueuePriorities = queuePriorites;
+
+    std::vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        VK_NV_RAY_TRACING_EXTENSION_NAME,
+        VK_EXT_DESCRIPTOR_INDEXING_EXTENSION_NAME
+    };
+
+    VkPhysicalDeviceDescriptorIndexingFeaturesEXT descriptorIndexing = { VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DESCRIPTOR_INDEXING_FEATURES_EXT };
+
+    VkPhysicalDeviceFeatures2 features2 = { VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2 };
+    features2.pNext = &descriptorIndexing;
+
+    vkGetPhysicalDeviceFeatures2(g_physicalDevice, &features2);
+
+    VkDeviceCreateInfo deviceCreateInfo = { VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
+    deviceCreateInfo.pNext = &features2;
+    deviceCreateInfo.flags = 0;
+    deviceCreateInfo.queueCreateInfoCount = 1;
+    deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
+    deviceCreateInfo.enabledLayerCount = 0;
+    deviceCreateInfo.ppEnabledLayerNames = nullptr;
+    deviceCreateInfo.enabledExtensionCount = uint32_t(deviceExtensions.size());
+    deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
+    deviceCreateInfo.pEnabledFeatures = nullptr;
+
+    VK_CHECK(vkCreateDevice(g_physicalDevice, &deviceCreateInfo, nullptr, &g_device));
+
+    vkGetDeviceQueue(g_device, g_queueIndex, 0, &g_queue);
+
+    g_rtProps = { VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_RAY_TRACING_PROPERTIES_NV };
+    g_rtProps.maxRecursionDepth = 0;
+    g_rtProps.shaderGroupHandleSize = 0;
+
+    VkPhysicalDeviceProperties2 deviceProperties2 = { VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2 };
+    deviceProperties2.pNext = &g_rtProps;
+
+    vkGetPhysicalDeviceProperties2(g_physicalDevice, &deviceProperties2);
+
+    VkWin32SurfaceCreateInfoKHR surfaceCreateInfo = { VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR };
+    surfaceCreateInfo.hinstance = GetModuleHandle(nullptr);
+    surfaceCreateInfo.hwnd = glfwGetWin32Window(g_window);
+
+    VK_CHECK(vkCreateWin32SurfaceKHR(g_instance, &surfaceCreateInfo, nullptr, &g_surface));
+
+    uint32_t surfaceFormatCount;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(g_physicalDevice, g_surface, &surfaceFormatCount, nullptr);
+    std::vector<VkSurfaceFormatKHR> surfaceFormats(surfaceFormatCount);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(g_physicalDevice, g_surface, &surfaceFormatCount, surfaceFormats.data());
+
+    // VK_FORMAT_B8G8R8A8_UNORM
+    bool surfaceFound = false;
+    for (auto& f : surfaceFormats)
+    {
+        if (f.format == VK_FORMAT_B8G8R8A8_UNORM)
+        {
+            g_surfaceFormat = f;
+            surfaceFound = true;
+            break;
+        }
+    }
+
+    if (!surfaceFound)
+        g_surfaceFormat = surfaceFormats[0];
+
+    VkSurfaceCapabilitiesKHR surfaceCapabilities;
+    VK_CHECK(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(g_physicalDevice, g_surface, &surfaceCapabilities));
+
+    VkPresentModeKHR presentMode = getPresentMode(g_physicalDevice, g_surface);
+
+    VkSwapchainKHR prevSwapchain = g_swapchain;
+
+    VkSwapchainCreateInfoKHR swapchainCreateInfo = { VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
+    swapchainCreateInfo.flags = 0;
+    swapchainCreateInfo.surface = g_surface;
+    swapchainCreateInfo.minImageCount = surfaceCapabilities.minImageCount;
+    swapchainCreateInfo.imageFormat = g_surfaceFormat.format;
+    swapchainCreateInfo.imageColorSpace = g_surfaceFormat.colorSpace;
+    swapchainCreateInfo.imageExtent.width = kWindowWidth;
+    swapchainCreateInfo.imageExtent.height = kWindowHeight;
+    swapchainCreateInfo.imageArrayLayers = 1;
+    swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+    swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    swapchainCreateInfo.queueFamilyIndexCount = 0;
+    swapchainCreateInfo.pQueueFamilyIndices = nullptr;
+    swapchainCreateInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+    swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    swapchainCreateInfo.presentMode = presentMode;
+    swapchainCreateInfo.clipped = VK_TRUE;
+    swapchainCreateInfo.oldSwapchain = prevSwapchain;
+
+    VK_CHECK(vkCreateSwapchainKHR(g_device, &swapchainCreateInfo, nullptr, &g_swapchain));
+
+    if (prevSwapchain)
+    {
+        for (auto& view : g_swapchainImageViews)
+            vkDestroyImageView(g_device, view, nullptr);
+        g_swapchainImageViews.clear();
+
+        // BONI TODO: need to destroy images?
+
+        vkDestroySwapchainKHR(g_device, prevSwapchain, nullptr);
+    }
+
+    uint32_t imageCount;
+    vkGetSwapchainImagesKHR(g_device, g_swapchain, &imageCount, nullptr);
+    g_swapchainImages.resize(imageCount);
+    vkGetSwapchainImagesKHR(g_device, g_swapchain, &imageCount, g_swapchainImages.data());
+
+    g_swapchainImageViews.resize(imageCount);
+    for (uint32_t i = 0; i < imageCount; i++)
+    {
+        VkImageViewCreateInfo imageViewCreateInfo = { VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO };
+        imageViewCreateInfo.format = g_surfaceFormat.format;
+        imageViewCreateInfo.subresourceRange = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 };
+        imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        imageViewCreateInfo.image = g_swapchainImages[i];
+        imageViewCreateInfo.flags = 0;
+        imageViewCreateInfo.components = {};
+
+        VK_CHECK(vkCreateImageView(g_device, &imageViewCreateInfo, nullptr, &g_swapchainImageViews[i]));
+    }
+
 
     return true;
 }
@@ -78,7 +259,10 @@ bool init()
 int main()
 {
     if (!init())
+    {
+        fprintf(stderr, "Error: unable to initialize Vulkan\n");
         return 1;
+    }
 
 
 
@@ -96,6 +280,8 @@ int main()
     while (!glfwWindowShouldClose(g_window)) {
         glfwPollEvents();
     }
+
+    vkDeviceWaitIdle(g_device);
 
     glfwDestroyWindow(g_window);
     glfwTerminate();
--- test1/src/pch.h
+++ test1/src/pch.h
@@ -8,8 +8,9 @@
 
 #include <volk.h>
 
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include "GLFW/glfw3.h"
+#include <GLFW/glfw3native.h>
 
 
 #define GLM_FORCE_RADIANS
