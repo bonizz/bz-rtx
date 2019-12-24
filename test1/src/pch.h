4e220fb1ecd74ddc11e7bee580515927d2ef2651
Boni <bonizz@gmail.com>
2019-12-24 06:39:15 -0800

    try to suppress unscoped enums
    This does not appear to suppress the warning in the analyzer.
    See: https://developercommunity.visualstudio.com/content/problem/724840/no-option-to-suppress-c-code-analysis-warning-c268.html
    
    Also, I'm not sure why it does not give the warning for all enums used.

--- test1/src/pch.h
+++ test1/src/pch.h
@@ -9,13 +9,14 @@
 #include <algorithm>
 #include <fstream>
 
-
+#pragma warning(push)
+#pragma warning(disable: 26812)
 #include <volk.h>
+#pragma warning(pop)
 
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include "GLFW/glfw3.h"
+#include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
-
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
