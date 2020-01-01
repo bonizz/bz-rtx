37395e23556cbc4ae6544556fe136148af4aab08
Boni <bonizz@gmail.com>
2020-01-01 07:00:28 -0800

    fix truncated log

--- test6/src/logging.cpp
+++ test6/src/logging.cpp
@@ -6,7 +6,7 @@ void DebugPrint(const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    size_t len = _vscprintf(fmt, args);
+    size_t len = _vscprintf(fmt, args) + 1;
     std::vector<char> buff(len);
     vsnprintf_s(buff.data(), len, _TRUNCATE, fmt, args);
     OutputDebugStringA(buff.data());
